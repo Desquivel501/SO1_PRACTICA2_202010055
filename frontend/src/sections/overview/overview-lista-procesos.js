9793e092dedc6373d78287845b20dc5b4fc656dd
Desquivel <3523114870101@ingenieria.usac.edu.gt>
2023-03-15 22:58:27 -0600

    Arreglos frontend

--- frontend/src/sections/overview/overview-lista-procesos.js
+++ frontend/src/sections/overview/overview-lista-procesos.js
@@ -28,25 +28,113 @@ const statusMap = {
   refunded: 'error'
 };
 
+
+function Row(props) {
+  const { order } = props;
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState([]);
+  // console.log(order)
+
+  const getList = event => {
+
+    let id = event.currentTarget.id
+    console.log(id)
+
+    fetch('http://127.0.0.1:5000/processes/' + id , {
+      method: 'GET',
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => {  
+        console.log(data.data)
+        setData(data.data)      
+    }).catch(console.error); 
+
+    setOpen(!open)
+  }
+
+  return (
+    <React.Fragment>
+      <TableRow
+        hover
+        key={order.PID}
+      >
+        <TableCell>
+          {order.PID}
+        </TableCell>
+        <TableCell>
+          {order.PROCESS_NAME}
+        </TableCell>
+        <TableCell>
+          {order.USERNAME}
+        </TableCell>
+        <TableCell>
+          {order.STATE}
+        </TableCell>
+        <TableCell>
+          {order.RAM}
+        </TableCell>
+
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={getList}
+            id={order.PID}
+          >
+            {open ? <FontAwesomeIcon icon={faChevronUp} size = '2x' /> : <FontAwesomeIcon icon={faChevronDown} size = '2x' />}
+          </IconButton>
+          
+        </TableCell>
+        
+      </TableRow>
+
+      <TableRow>
+      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <Box sx={{ margin: 1 }}>
+            <Typography variant="h6" gutterBottom component="tr">
+              Procesos Hijos
+            </Typography>
+
+            <TableContainer sx={{ maxHeight: 400 }}>
+            <Table size="small" stickyHeader>
+              <TableHead>
+                <TableRow>
+                  <TableCell>PID</TableCell>
+                  <TableCell>Proceso</TableCell>
+                  <TableCell>Usuario</TableCell>
+                  <TableCell>Estado</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+
+                {data.map((val) => (
+                  <TableRow key={val.PID}>
+                    <TableCell>{val.PID}</TableCell>
+                    <TableCell>{val.PROCESS_NAME}</TableCell>
+                    <TableCell>{val.USERNAME}</TableCell>
+                    <TableCell>{val.STATE}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            </TableContainer>            
+    
+          </Box>
+        </Collapse>
+      </TableCell>
+    </TableRow>
+
+    </React.Fragment>
+  )
+}
+
+
 export const OverviewListaProcesos = (props) => {
   const { orders = [], sx } = props;
-
-  // const [orders, setOrders] = useState([]) 
-
-  // useEffect(() => {
-  //   fetch('http://127.0.0.1:5000/processes', {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type':'application/json'
-  //     }
-  //   })
-  //   .then(resp => resp.json())
-  //   .then(data => {  
-  //     setOrders(data.data)
-  //     console.log(orders)         
-  //   }).catch(console.error); 
-  // },[]);
-  
 
   return (
     <Card sx={sx}>
@@ -77,108 +165,12 @@ export const OverviewListaProcesos = (props) => {
             </TableHead>
 
             <TableBody>
-              {orders.map((order) => {
-                const [open, setOpen] = useState(false);
-                const [data, setData] = useState([]);
-                // console.log(order)
+              {
+                orders.map((order) => (
+                    <Row key={order.PID} row={order} />
+                ))
+              }
 
-                const getList = event => {
-
-                  let id = event.currentTarget.id
-                  console.log(id)
-
-                  fetch('http://127.0.0.1:5000/processes/' + id , {
-                    method: 'GET',
-                    headers: {
-                      'Content-Type':'application/json'
-                    }
-                  })
-                  .then(resp => resp.json())
-                  .then(data => {  
-                      console.log(data.data)
-                      setData(data.data)      
-                  }).catch(console.error); 
-
-                  setOpen(!open)
-                }
-                
-                return (
-                  <React.Fragment>
-                    <TableRow
-                      hover
-                      key={order.PID}
-                    >
-                      <TableCell>
-                        {order.PID}
-                      </TableCell>
-                      <TableCell>
-                        {order.PROCESS_NAME}
-                      </TableCell>
-                      <TableCell>
-                        {order.USERNAME}
-                      </TableCell>
-                      <TableCell>
-                        {order.STATE}
-                      </TableCell>
-                      <TableCell>
-                        {order.RAM}
-                      </TableCell>
-
-                      <TableCell>
-                        <IconButton
-                          aria-label="expand row"
-                          size="small"
-                          onClick={getList}
-                          id={order.PID}
-                        >
-                          {open ? <FontAwesomeIcon icon={faChevronUp} size = '2x' /> : <FontAwesomeIcon icon={faChevronDown} size = '2x' />}
-                        </IconButton>
-                        
-                      </TableCell>
-                      
-                    </TableRow>
-
-                    <TableRow>
-                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                      <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box sx={{ margin: 1 }}>
-                          <Typography variant="h6" gutterBottom component="tr">
-                            Procesos Hijos
-                          </Typography>
-
-                          <TableContainer sx={{ maxHeight: 400 }}>
-                          <Table size="small" stickyHeader>
-                            <TableHead>
-                              <TableRow>
-                                <TableCell>PID</TableCell>
-                                <TableCell>Proceso</TableCell>
-                                <TableCell>Usuario</TableCell>
-                                <TableCell>Estado</TableCell>
-                              </TableRow>
-                            </TableHead>
-                            <TableBody>
-
-                              {data.map((val) => (
-                                <TableRow key={val.PID}>
-                                  <TableCell>{val.PID}</TableCell>
-                                  <TableCell>{val.PROCESS_NAME}</TableCell>
-                                  <TableCell>{val.USERNAME}</TableCell>
-                                  <TableCell>{val.STATE}</TableCell>
-                                </TableRow>
-                              ))}
-                            </TableBody>
-                          </Table>
-                          </TableContainer>            
-                  
-                        </Box>
-                      </Collapse>
-                    </TableCell>
-                  </TableRow>
-
-                  </React.Fragment>
-                  
-                );
-              })}
             </TableBody>
           </Table>
         </TableContainer>
